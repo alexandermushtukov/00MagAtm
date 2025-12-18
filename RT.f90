8511b9a78f618e36ddfde1759a89fad4d0d173d8
alexander_mushtukov <31170808+alexandermushtukov@users.noreply.github.com>
2025-12-18 15:25:35 +0000

    Update RT.f90

--- RT.f90
+++ RT.f90
@@ -4,11 +4,11 @@
 subroutine set_atm_structure()
 implicit none
 real*8::pi=3.141592653589793d0
-real*8,allocatable::m_atm_I(:,:,:,:),m_atm_T_rho(:,:)
-complex*16,allocatable::m_atm_S(:,:,:,:,:,:,:)
+real*8,allocatable::m_atm_I(:,:,:,:),m_atm_T_rho(:,:),m_atm_sigma_abs(:,:,:)
+complex*16,allocatable::m_atm_S(:,:,:,:,:,:)
 real*8::B12,m_ns,R6,g14,theta_b,m_max,E,E_cyc
 integer::n_m,n_mu,n_fi
-real*8::dmu,dfi,mu_i,mu_f,theta_i,theta_f,theta_ib,theta_fb,delta_fi,fi_i,fi_f,fi_ib,fi_fb,n_ib(3),n_fb(3),n_i(3),n_f(3)
+real*8::dmu,dfi,mu_i,mu_f,theta_i,theta_f,theta_ib,theta_fb,fi_i,fi_f,fi_ib,fi_fb,n_ib(3),n_fb(3),n_i(3),n_f(3)
 real*8::help
 integer::i,j,j1,j2,k1,k2
 complex*16::Amp_dSigmadOmega !== functions ==!
@@ -29,17 +29,16 @@ complex*16::Amp_dSigmadOmega !== functions ==!
   n_mu = 20;  n_fi = 20
   !=========================!
 
-  allocate( m_atm_I(n_m,n_mu,n_fi,2),m_atm_T_rho(n_m,2),m_atm_S(n_m,n_mu,n_mu,n_fi,n_fi,2,2) )  !== intensity; T & rho; S-matrix ==!
+  allocate( m_atm_I(n_m,n_mu,n_fi,2),m_atm_T_rho(n_m,2),m_atm_S(n_mu,n_mu,n_fi,n_fi,2,2) )  !== intensity; T & rho; S-matrix ==!
+  allocate( m_atm_sigma_abs(n_m,n_mu,n_fi) )   !== free-free absorption ==!
   dmu = 2.d0/n_mu; dfi = 2*pi/n_fi
 
   j1 = 1
   do while( j1 .le. n_mu )
-    mu_i = dmu/2 + (j1-1)*dmu
-    theta_i = acos(mu_i)
+    mu_i = dmu/2 + (j1-1)*dmu; theta_i = acos(mu_i)
     j2 = 1
     do while( j2 .le. n_mu )
-      mu_f = dmu/2 + (j2-1)*dmu
-      theta_f = acos(mu_f)
+      mu_f = dmu/2 + (j2-1)*dmu; theta_f = acos(mu_f)
       k1 = 1
       do while( k1 .le. n_fi )
         fi_i = dfi/2 + (k1-1)*dfi
@@ -52,10 +51,10 @@ complex*16::Amp_dSigmadOmega !== functions ==!
           call spherical2cartisian(1.d0,theta_f,fi_f,n_f(1),n_f(2),n_f(3))
           call VecRotation3d( n_f(1),n_f(2),n_f(3), 1, -theta_b , n_fb(1),n_fb(2),n_fb(3))
           call cartesian2spherical( n_fb(1),n_fb(2),n_fb(3) , help,theta_fb,fi_fb )
-          m_atm_S(1:n_m,j1,j2,k1,k2,1,1) = Amp_dSigmadOmega(1,1,E,E_cyc,theta_ib,theta_fb,fi_ib,fi_fb)
-          m_atm_S(1:n_m,j1,j2,k1,k2,1,2) = Amp_dSigmadOmega(1,2,E,E_cyc,theta_ib,theta_fb,fi_ib,fi_fb)
-          m_atm_S(1:n_m,j1,j2,k1,k2,2,1) = Amp_dSigmadOmega(2,1,E,E_cyc,theta_ib,theta_fb,fi_ib,fi_fb)
-          m_atm_S(1:n_m,j1,j2,k1,k2,2,2) = Amp_dSigmadOmega(2,2,E,E_cyc,theta_ib,theta_fb,fi_ib,fi_fb)
+          m_atm_S(j1,j2,k1,k2,1,1) = Amp_dSigmadOmega(1,1,E,E_cyc,theta_ib,theta_fb,fi_ib,fi_fb)
+          m_atm_S(j1,j2,k1,k2,1,2) = Amp_dSigmadOmega(1,2,E,E_cyc,theta_ib,theta_fb,fi_ib,fi_fb)
+          m_atm_S(j1,j2,k1,k2,2,1) = Amp_dSigmadOmega(2,1,E,E_cyc,theta_ib,theta_fb,fi_ib,fi_fb)
+          m_atm_S(j1,j2,k1,k2,2,2) = Amp_dSigmadOmega(2,2,E,E_cyc,theta_ib,theta_fb,fi_ib,fi_fb)
           !write(*,*)m_atm_S(4,j1,j2,k1,k2,1:2,1:2); read(*,*)
           k2 = k2+1
         end do
