47fc3539fa53deb63691e58f0b0627495d2b8413
alexander_mushtukov <31170808+alexandermushtukov@users.noreply.github.com>
2025-12-18 13:45:27 +0000

    Update RT.f90

--- RT.f90
+++ RT.f90
@@ -1,4 +1,5 @@
 !========================================================================================
+!  m_atm_S(:,:,:,:,:) - scattering matrix, (n_m,n_mu,n_mu,2*n_fi,4)
 !========================================================================================
 subroutine set_atm_structure()
 implicit none
@@ -19,23 +20,19 @@ integer::i,j,j1,j2,k
   n_fi = 40
   allocate( m_atm_I(n_m,n_mu,n_fi,2),m_atm_T_rho(n_m,2),m_atm_S(n_m,n_mu,n_mu,2*n_fi,4) )  !== intensity; T & rho; S-matrix ==!
   dmu = 2.d0/n_mu; dfi = pi/n_fi
-  i = 1
-  do while( i.le.n_m )
-    j1 = 1
-    do while( j1 .le. n_mu )
-      j2 = 1
-      do while( j2 .le. n_mu )
-        k = 1
-        do while( k .le. 2*n_fi )
-          m_atm_S(i,j1,j2,k,1:4) = (0.d0,1.d0)
-          k = k+1
-        end do
-        j2 = j2+1
+  j1 = 1
+  do while( j1 .le. n_mu )
+    j2 = 1
+    do while( j2 .le. n_mu )
+      k = 1
+      do while( k .le. 2*n_fi )
+        m_atm_S(1:n_m,j1,j2,k,1:4) = (0.d0,1.d0)
+        k = k+1
       end do
-      j1 = j1+1
+      j2 = j2+1
     end do
-    write(*,*)i
-    i = i+1
+    write(*,*)j1,n_mu
+    j1 = j1+1
   end do
 return
 end subroutine set_atm_structure
