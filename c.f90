244ba34658effb6c6271c2bccb86349b1dcb06a1
alexander_mushtukov <31170808+alexandermushtukov@users.noreply.github.com>
2026-01-28 12:13:32 +0000

    Update c.f90

--- c.f90
+++ c.f90
@@ -3,7 +3,21 @@
 program MagAtm
 implicit none
 real*8::pi=3.141592653589793d0
-  write(*,*)"###"
-  call set_atm_structure()
+real*8::E,B12,m_ns,R6,theta_B,Z,A,dot_m_6,ln_Lambda
+  write(*,*)"#1"
+  !== physical paramters ==!
+  E = 2.d0
+  m_ns = 1.4d0
+  R6 = 1.d0
+  B12 = 1.d2
+  theta_B = 0.5d0
+  Z = 1.d0
+  A = 1.d0
+  dot_m_6 = 0.d0
+  ln_Lambda = 10.d0
+  !==========================!
+
+  call pol_RT_fixE(E,B12,m_ns,R6,theta_B,Z,A,dot_m_6,ln_Lambda)
+  write(*,*)"#2"
 return
 end program MagAtm
