41c59aa27b7f2b283b7bdbc892e92b8366c9dc40
alexander_mushtukov <31170808+alexandermushtukov@users.noreply.github.com>
2026-03-18 13:32:30 +0000

    Update c.f90

--- c.f90
+++ c.f90
@@ -5,12 +5,12 @@ use omp_lib
 use black_body
 implicit none
 real*8::pi=3.141592653589793d0
-real*8::E,B12,m_ns,R6,theta_B,Z,A,dot_m_6,ln_Lambda,T_eff    !== physical parameters ==!
+real*8::E,E_,B12,m_ns,R6,theta_B,Z,A,dot_m_6,ln_Lambda,T_eff    !== physical parameters ==!
 real*8::m_min,m_max,kappa_T,tau_min,tau_max
 integer::n_m,n_mu,n_fi,i,n_E
 real*8::g14,E_min,E_max,dE,F_E_target,F_target,flux,alpha,T_old,T_corr,T_floor
 real*8,allocatable::mas_tau_TkeV(:,:),mas_m_rho(:,:),flux_tot(:,:),flux_E(:,:),J_E(:,:),kabs_mean(:,:),spec(:,:),num(:,:),den(:,:),&
-                    B(:),dBdT(:)
+                    B(:),dBdT(:),mas_tau_TkeV_new(:,:)
 integer::n_stream,i_task,i_iter,jj
 character*100::file_sp,file_t
 real*8::eps,dT
@@ -62,8 +62,8 @@ real*8::sigma_SB_22_keV
   tau_min = m_min*kappa_T
   tau_max = m_max*kappa_T
 
-  allocate( mas_tau_TkeV(n_m,2),mas_m_rho(n_m,2),flux_tot(n_m,2),flux_E(n_m,2),J_E(n_m,2),kabs_mean(n_m,2),spec(n_E,3),&
-            num(n_m,2),den(n_m,2),B(n_m),dBdT(n_m))
+  allocate( mas_tau_TkeV(n_m,2),mas_tau_TkeV_new(n_m,2),mas_m_rho(n_m,2),flux_tot(n_m,2),flux_E(n_m,2),J_E(n_m,2),&
+            kabs_mean(n_m,2),spec(n_E,3),num(n_m,2),den(n_m,2),B(n_m),dBdT(n_m))
   !== set up some initial temperature sctructure ==!
   i = 1
   do while( i.le. n_m )
@@ -74,9 +74,9 @@ real*8::sigma_SB_22_keV
 
   !== teperature iterrrations ==!
   i_iter = 0
-  do while(i_iter.le.6)
+  do while(i_iter.le.500)
     !== get hydro structure of the atmosphere ==!
-    write(*,*)"# start iter:",i_iter
+    write(*,*)"# start iter: i,flux_E(1,1:2)"
     call get_hydro_atm_structure(mas_m_rho,g14,dot_m_6,ln_Lambda,tau_min,tau_max,n_m,mas_tau_TkeV)
     !write(*,*)"#a1"
     F_target = 0.d0
@@ -87,7 +87,7 @@ real*8::sigma_SB_22_keV
     !== integration over the energy band ==!
     i = 1
     do while(i.le.n_E)
-      E = E_min * (E_max/E_min)**( real(i-1) / real(n_E-1) )
+      E  = E_min * (E_max/E_min)**( real(i-1) / real(n_E-1) )
       F_E_target = pi * BB_Intensity_22(E,T_eff)
       dE  = E*( (E_max/E_min)**(1.0/(n_E-1)) - 1.0 )
       call pol_RT_fixE(flux_E,J_E,kabs_mean,E,B12,g14,T_eff,theta_B,Z,A,dot_m_6,ln_Lambda,mas_m_rho,mas_tau_TkeV,n_m,n_mu,n_fi)
@@ -113,32 +113,38 @@ real*8::sigma_SB_22_keV
       i = i+1
     end do
 
-    i = 1
-    do while(i.le.n_m)
-      !flux = flux_tot(i,1)+flux_tot(i,2)
-      !if( flux.le.0.d0 )then
-      !  flux = F_target/2
-      !end if
+    alpha = 0.15d0   !== damping ==!
 
-      !== temperature correction ==!
-      alpha = 0.3d0   !== damping ==!
-      T_old  = mas_tau_TkeV(i,2)
-      !!T_corr = mas_tau_TkeV(i,2) * ( F_target/ flux )**0.25
-      !!!T_corr = T_old * (F_goal/flux)**0.25
-      !! mas_tau_TkeV(i,2)  = (1d0-alpha)*T_old + alpha*T_corr
-      !!!mas_tau_TkeV(i,2) = mas_tau_TkeV(i,2) * ( F_target/ flux )**0.25
+    !== temperature correction A ==!
+    !i = 1
+    !do while(i.le.n_m)
+    !  !flux = flux_tot(i,1)+flux_tot(i,2)
+    !  !if( flux.le.0.d0 )then
+    !  !  flux = F_target/2
+    !  !end if!
+
+    !  T_old  = mas_tau_TkeV(i,2)
+    !  !!T_corr = mas_tau_TkeV(i,2) * ( F_target/ flux )**0.25
+    !  !!!T_corr = T_old * (F_goal/flux)**0.25
+    !  !! mas_tau_TkeV(i,2)  = (1d0-alpha)*T_old + alpha*T_corr
+    !  !!!mas_tau_TkeV(i,2) = mas_tau_TkeV(i,2) * ( F_target/ flux )**0.25
       
-      if ( (den(i,1)+den(i,2)).gt.0. ) then
-        dT = - (num(i,1)+num(i,2)) / (den(i,1)+den(i,2))
-        mas_tau_TkeV(i,2) = T_old + alpha * dT
-        mas_tau_TkeV(i,2) = max( T_floor, mas_tau_TkeV(i,2) )  !== twemperature cannot drop below some level ==!
-        !if( mas_tau_TkeV(i,2).le.0.d0  )then
-        !  write(*,*)"T<0",mas_tau_TkeV(i,2),dT; read(*,*)
-        !end if
-      end if
-      write(*,*)i,flux_tot(i,1:2),F_target,sigma_SB_22_keV*T_eff**4," $$$ ",mas_tau_TkeV(i,2)
-      i = i+1
-    end do
+    !  if ( (den(i,1)+den(i,2)).gt.0. ) then
+    !    dT = - (num(i,1)+num(i,2)) / (den(i,1)+den(i,2))
+    !    dT = max( -0.1d0*T_old, min( 0.1d0*T_old, dT ) )
+    !    mas_tau_TkeV(i,2) = T_old + alpha * dT
+    !    mas_tau_TkeV(i,2) = max( T_floor, mas_tau_TkeV(i,2) )  !== twemperature cannot drop below some level ==!
+    !    !if( mas_tau_TkeV(i,2).le.0.d0  )then
+    !    !  write(*,*)"T<0",mas_tau_TkeV(i,2),dT; read(*,*)
+    !    !end if
+    !  end if
+    !  write(*,*)i,flux_tot(i,1:2),F_target,sigma_SB_22_keV*T_eff**4," $$$ ",mas_tau_TkeV(i,2)
+    !  i = i+1
+    !end do
+
+    call temperature_correction_1(mas_tau_TkeV_new,n_m,flux_tot,mas_tau_TkeV,F_target,alpha,T_floor)
+    mas_tau_TkeV(1:n_m,1:2) = mas_tau_TkeV_new(1:n_m,1:2)
+
     i_iter = i_iter+1
   end do
   write(*,*)"# iteration ended"
@@ -175,3 +181,31 @@ real*8::sigma_SB_22_keV
   !$omp end parallel
 144 return
 end program MagAtm
+
+
+!==============================================================================================
+!==============================================================================================
+subroutine temperature_correction_1(mas_tau_TkeV_new,n_m,flux_tot,mas_tau_TkeV,F_target,alpha,T_floor)
+implicit none
+real*8,intent(out)::mas_tau_TkeV_new(n_m,2)
+integer,intent(in)::n_m
+real*8,intent(in)::flux_tot(n_m,2),mas_tau_TkeV(n_m,2),alpha,F_target,T_floor
+integer::i
+real*8::flux,T_old,T_corr
+  mas_tau_TkeV_new(1:n_m,1:2) = mas_tau_TkeV(1:n_m,1:2)
+  i = 1
+  do while(i.le.n_m)
+    flux = flux_tot(i,1) + flux_tot(i,2)
+    T_old = mas_tau_TkeV(i,2)
+
+    if (flux .gt. 1d-30) then
+      T_corr = T_old * (F_target/flux)**0.25d0
+      mas_tau_TkeV_new(i,2) = (1.d0-alpha)*T_old + alpha*T_corr
+      mas_tau_TkeV_new(i,2) = max(T_floor, mas_tau_TkeV_new(i,2))
+    end if
+    !write(*,*)i,mas_tau_TkeV(i,2),F_target/flux,F_target,flux
+    i = i + 1
+  end do
+return
+end subroutine temperature_correction_1
+
